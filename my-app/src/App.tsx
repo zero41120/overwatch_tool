fd3555a008cb247cd05d6aea1d87a84edad5f153
YSH <devtlin34@gmail.com>
2025-06-12 02:24:59 -0700

    feat: Add overwatch stadium shop like background

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,16 @@
 import Optimizer from './Optimizer';
+import BreakPointCalculator from './components/BreakPointCalculator';
 import Toolbar from './components/Toolbar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-teal-50 to-blue-100 p-6 text-gray-800">
-      <div className="max-w-5xl mx-auto">
-        <Toolbar />
-        <Optimizer />
+    <div className="h-screen overflow-hidden text-gray-800 p-2 box-border">
+      <div className="border border-[#346291] rounded-md h-full w-full">
+        <div className="max-w-5xl mx-auto grid gap-6 h-full overflow-hidden">
+          <Toolbar />
+          <Optimizer />
+          <BreakPointCalculator />
+        </div>
       </div>
     </div>
   );
