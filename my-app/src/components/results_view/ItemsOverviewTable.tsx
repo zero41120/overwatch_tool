5ca383b3c73fd0678db550d0d367438d26ac7fb2
YSH <devtlin34@gmail.com>
2025-07-02 03:39:11 -0700

    fix: improve item display and tooltip handling in ItemsOverviewTable

--- my-app/src/components/results_view/ItemsOverviewTable.tsx
+++ my-app/src/components/results_view/ItemsOverviewTable.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch } from "../../hooks";
-import { setTooltip, clearTooltip } from "../../slices/tooltipSlice";
+import { clearTooltip, setTooltip } from "../../slices/tooltipSlice";
 import type { Item } from "../../types";
 import { sortItemsOverview } from "../../utils/item";
 import { rarityColor } from "../../utils/utils";
@@ -25,27 +25,39 @@ export default function ItemsOverviewTable({ eqItems, resultItems, showHeader = 
           return (
             <div
               key={idx}
-              className="flex items-center border border-gray-200 dark:border-gray-700 p-1"
+              className={`flex items-center border border-gray-200 dark:border-gray-700 p-1 min-h-12 ${!it ? "bg-gray-200 dark:bg-gray-800" : ""
+                }`}
               style={{ color: it ? rarityColor(it.rarity) : undefined }}
-              onMouseEnter={(e) =>
+              onMouseEnter={e =>
                 it && dispatch(setTooltip({ item: it, x: e.clientX, y: e.clientY }))
               }
-              onMouseMove={(e) =>
+              onMouseMove={e =>
                 it && dispatch(setTooltip({ item: it, x: e.clientX, y: e.clientY }))
               }
               onMouseLeave={() => dispatch(clearTooltip())}
             >
-              {it && (
+              {it ? (
                 <>
                   {it.iconUrl && <img src={it.iconUrl} alt="" className="w-10 h-10" />}
-                  <div className="ml-2 flex-1 break-words text-sm leading-tight">{it.name}</div>
+                  <div
+                    className="ml-2 flex-1 text-sm leading-tight overflow-hidden max-w-[7rem] break-words"
+                    style={{
+                      display: '-webkit-box',
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: 'vertical',
+                      wordBreak: 'break-all',
+                      hyphens: 'auto'
+                    }}
+                    title={it.name}
+                  >
+                    {it.name}
+                  </div>
                 </>
-              )}
+              ) : null}
             </div>
           );
         })}
       </div>
-
     </div>
   );
 }
