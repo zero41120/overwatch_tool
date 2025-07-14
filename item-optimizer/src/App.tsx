357182205bf162147317c1da4c24880007dcd077
YSH <devtlin34@gmail.com>
2025-07-13 22:34:06 -0700

    Adjust padding for App and Optimizer components

--- item-optimizer/src/App.tsx
+++ item-optimizer/src/App.tsx
@@ -3,7 +3,7 @@ import Tooltip from "./components/shared/Tooltip";
 
 function App() {
   return (
-    <div className="dark h-screen overflow-hidden dark:text-gray-100 p-1 sm:p-2 box-border">
+    <div className="dark h-screen overflow-hidden dark:text-gray-100 p-0 box-border">
       <div className="border border-[#346291] rounded-md h-full w-full">
         <div className="mx-auto grid gap-6 h-full overflow-y-auto">
           <Optimizer />
